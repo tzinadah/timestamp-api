7030fe708592704e9882a385be7f6984b992a923
Talal Abuziandah <primezinadah@gmail.com>
2024-10-17 14:13:51 +0100

    Add api get methods

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(PORT);
-console.log(`Server is listening to port ${PORT}`);
+app.get("/api/:date?", (req, res) => {
+  const date = req.params.date ? isNaN(Number(req.params.date)) ? new Date(req.params.date) : new Date(Number(req.params.date)) : new Date();
+  isNaN(date.getTime()) ? res.json({error: "Invalid Date"}) : res.json({unix: date.getTime(), utc: date.toUTCString()})
+});
+
+app.listen(PORT, (err, res) => {
+  if (!err) console.log(`Server is listening to port ${PORT}`);
+});
